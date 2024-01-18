c3301cb8e1f7a4b22a36b7783e090513cff8a3f0
Beatrizbms <157053082+Beatrizbms@users.noreply.github.com>
2024-01-18 15:58:43 -0200

    Initial commit

---
+++ coias.py
@@ -0,0 +1,4 @@
+nome = print(input('Escreva seu nome: '))
+#if nome == 'Beatriz' or 'beah' or 'beatriz':
+#    print('Seu nome é lindo!!!')
+str(print('Olá {}, seja em vindo(a). Não tem nada aqui ainda, ent tchau'.format(nome)))  
